3b66a2461255a2a0911192d3db276e9c1f1c7f71
Vivek Tiwari <vivekdtiwari@gmail.com>
2016-05-08 17:44:35 +0530

    First test React component.

---
+++ app/components/HelloTest.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+
+var Main = React.createClass({
+  render : function(){
+    return (
+      <div className="test-container">
+      React functionality
+      </div>
+    )
+  }
+})
+
+module.exports = Main;
